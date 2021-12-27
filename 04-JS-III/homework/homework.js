580bfeaba4e85a0785cf8114d99efb8fded1912f
mario <mariostreri@gmail.com>
2021-12-27 00:12:04 -0300

    homework.js 04-js-3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -249,7 +249,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  
+  var arrayNum = [];
+
+  for(var i=0; i<10; i++){
+    numero = numero+2
+    
+    if(numero === i){
+        break;
+    }else{
+      arrayNum.push(numero)
+    }
+  }
+  if(i<10){
+    return "Se interrumpió la ejecución";
+  }else{
+  return arrayNum;
+  }
 }
 
 
@@ -260,8 +275,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-}
 
+  var array=[];
+
+  for(var i = 0;i<10;i++){
+    if(i === 5){
+      continue;
+    } else{
+      numero = numero + 2;
+    array.push(numero);
+    }
+  }
+  return array;
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
