a2677b6948a855fc9aab2d3ef415abefbf68c4bf
mario <mariostreri@gmail.com>
2022-01-07 17:17:05 -0300

    homework.js-05-JS-4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -113,6 +113,10 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+
+  usuario.amigos.push(nuevoAmigo);
+
+    return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -121,8 +125,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for(var i = 0; i<usuarios.length ; i++){
+       usuarios[i]["esPremium"] = true;
+  }
+ return usuarios;
 }
-
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
@@ -130,6 +137,12 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+
+  for(var i =0 ; i<usuario.posts.length ; i++){
+    suma = suma + usuario.posts[i].likes;
+
+  }return suma;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -142,7 +155,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+    return this["precio"] - ( this["precio"] * this["porcentajeDeDescuento"] );
+  };
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
